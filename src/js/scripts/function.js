3e94d11dc2510e7670bd2b82fb7325e3dfd4442b
Stanislav Bakryu <stasbakryu@gmail.com>
2021-01-09 22:18:20 +0200

    create tab menu

--- src/js/scripts/function.js
+++ src/js/scripts/function.js
@@ -1,5 +1,9 @@
-const fn = (...args) => {
-  const res = 123123;
-  args.forEach((item) => console.log(item));
-};
-export default fn;
+const accordion = document.querySelector(".accordion");
+accordion.addEventListener("click", handleChange);
+function handleChange({ target }) {
+  const accordionContent = target.parentNode.querySelector(
+    ".accordion__content"
+  );
+  console.log(window.getComputedStyle(accordionContent).height);
+  accordionContent.classList.toggle("closed");
+}
